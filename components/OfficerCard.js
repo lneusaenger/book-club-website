2f295712320744213488e10cfcfa3f9703203117
Lily Neusaenger <lily.neusaenger@duke.edu>
2023-03-01 17:56:57 -0500

    Fixed covers

--- components/OfficerCard.js
+++ components/OfficerCard.js
@@ -1,39 +1,96 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGoodreads } from "@fortawesome/free-brands-svg-icons"
 import { Card, Col, Row } from 'antd';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import FavCard from "./FavCard";
 
-function OfficerCard (props){
-    const { Meta } = Card;
-const tabList = [
-  {
-    key: 'fav1',
-    tab: 'Favorite #1',
-  },
-  {
-    key: 'fav2',
-    tab: 'Favorite #2',
-  },
-  {
-    key: 'fav3',
-    tab: 'Favorite #3',
-  },
-  {
-    key: 'fav4',
-    tab: 'Favorite #4',
-  },
-  {
-    key: 'fav5',
-    tab: 'Favorite #5',
-  },
-];
-const contentList = {
-  fav1: <p>first favorite book</p>,
-  fav2: <p>second favorite book</p>,
-  fav3: <p>third favorite book</p>,
-  fav4: <p>fourth favorite book</p>,
-  fav5: <p>fifth favorite book</p>,
-};
+function OfficerCard(props) {
+    const [favBooks, setFavBooks] = useState([]);
+    useEffect(() => {
+      const getFavs = async() =>{
+        try {
+          const response = await fetch(props.favs);
+          const data = await response.json();
+          const books = data.editions.slice(0, 5);
+          setFavBooks(books);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      getFavs();
+    }, [props.favs]);
+  
+    const tabList = [
+      {
+        key: "fav1",
+        tab: "Favorite #1",
+      },
+      {
+        key: "fav2",
+        tab: "Favorite #2",
+      },
+      {
+        key: "fav3",
+        tab: "Favorite #3",
+      },
+      {
+        key: "fav4",
+        tab: "Favorite #4",
+      },
+      {
+        key: "fav5",
+        tab: "Favorite #5",
+      },
+    ];
+  
+    const contentList = {
+        fav1: favBooks[0] ? (
+          <FavCard
+            title={favBooks[0].title}
+            cover={Array.isArray(favBooks[0].isbn_13) ? 
+                `https://covers.openlibrary.org/b/isbn/${favBooks[0].isbn_13[0]}-L.jpg` : 
+                `https://covers.openlibrary.org/b/isbn/${favBooks[0].isbn_13}-L.jpg`}
+            author={favBooks[0].author}
+          />
+        ) : null,
+        fav2: favBooks[1] ? (
+          <FavCard
+            title={favBooks[1].title}
+            cover={Array.isArray(favBooks[1].isbn_13) ? 
+                `https://covers.openlibrary.org/b/isbn/${favBooks[1].isbn_13[0]}-L.jpg` : 
+                `https://covers.openlibrary.org/b/isbn/${favBooks[1].isbn_13}-L.jpg`}
+            author={favBooks[1].author}
+          />
+        ) : null,
+        fav3: favBooks[2] ? (
+          <FavCard
+            title={favBooks[2].title}
+            cover={Array.isArray(favBooks[2].isbn_13) ? 
+                `https://covers.openlibrary.org/b/isbn/${favBooks[2].isbn_13[0]}-L.jpg` : 
+                `https://covers.openlibrary.org/b/isbn/${favBooks[2].isbn_13}-L.jpg`}
+            author={favBooks[2].author}
+          />
+        ) : null,
+        fav4: favBooks[3] ? (
+          <FavCard
+            title={favBooks[3].title}
+            cover={Array.isArray(favBooks[3].isbn_13) ? 
+                `https://covers.openlibrary.org/b/isbn/${favBooks[3].isbn_13[0]}-L.jpg` : 
+                `https://covers.openlibrary.org/b/isbn/${favBooks[3].isbn_13}-L.jpg`}
+            author={favBooks[3].author}
+          />
+        ) : null,
+        fav5: favBooks[4] ? (
+          <FavCard
+            title={favBooks[4].title}
+            cover={Array.isArray(favBooks[4].isbn_13) ? 
+                `https://covers.openlibrary.org/b/isbn/${favBooks[4].isbn_13[0]}-L.jpg` : 
+                `https://covers.openlibrary.org/b/isbn/${favBooks[4].isbn_13}-L.jpg`}
+            author={favBooks[4].author}
+          />
+        ) : null,
+      };
+
   const [activeTabKey, setActiveTabKey] = useState('app');
   const onTabChange = (key) => {
     setActiveTabKey(key);
