af30695f28e42203e0579a04d72abe822afb33d3
Lily Neusaenger <lily.neusaenger@duke.edu>
2023-03-09 17:27:34 -0500

    new colors & font (preview)

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,10 @@ export default function HomePage() {
     <>
       <Navbar />
       <div className = "welcome">
-      <span className = "container-welcome">
-        <img className = "welcome-img" src = "https://cdn.pixabay.com/photo/2017/08/21/04/13/books-2664021_960_720.png"></img>
-        </span>
-        <span className = "welcome-message">
-          <h1 className = "message">DUKE'S EXTREMELY PRETENIOUS BOOK CLUB</h1>
-        </span>
-        </div>
+        <div className = "welcome-message">
+          Duke's Extremely Pretentious Book Club
+          </div>
+      </div>
         <Footer/>
     </>
   );
